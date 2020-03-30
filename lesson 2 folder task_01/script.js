2781d262adbeb412bdd11848aabb4c832339c736
Boris Fokin <Befokin@mail.ru>
2020-03-30 17:16:55 +0300

    выполнен урок 2, задание 1

--- lesson 2 folder task_01/script.js
+++ lesson 2 folder task_01/script.js
@@ -1,1 +1,22 @@
-jfldhdfh, опоарв
+let money = 1000;
+let income = '500';
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 10000;
+let period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('period =' + ' ' + period + ' ' + 'месяцев');
+console.log('цель заработвть' + ' '+ mission + ' ' + 'долларов');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+
+let budgetDay = (money/30);
+console.log(budgetDay);
