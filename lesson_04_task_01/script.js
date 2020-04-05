cfde1650d64fb8b37f179e624bcdc32c1a6869f6
Boris Fokin <Befokin@mail.ru>
2020-04-05 19:25:42 +0300

    перезалил по новой Lesson_04 task_01

--- lesson_04_task_01/script.js
+++ lesson_04_task_01/script.js
@@ -67,13 +67,13 @@ console.log('Бюджет на день:  ' + Math.floor(budgetDay));
 // Задание №9
 let getStatusIncome = function() {
 if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
+    return ('У вас высокий уровень дохода');
 } else if ((budgetDay >= 600) && (budgetDay <= 1200)) {
-    console.log('У вас средний уровень дохода');
+    return ('У вас средний уровень дохода');
 } else if ((budgetDay < 600) && (budgetDay >= 0)) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
+    return ('К сожалению у вас уровень дохода ниже среднего');
 } else {
-    console.log('Что то пошло не так');
+    return('Что то пошло не так');
 } 
 }
 console.log(getStatusIncome());
