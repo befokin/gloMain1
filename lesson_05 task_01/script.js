63d5c7c995656529876dc7c459ed6c89dbc21b87
Boris Fokin <Befokin@mail.ru>
2020-04-05 23:12:09 +0300

    обновил lesson_05 task_01

---
+++ lesson_05 task_01/script.js
@@ -0,0 +1,118 @@
+'use strict';
+
+let money = 100000;
+let income = '5000';
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 10000;
+let period = 12;
+
+let showTypeOf = function(name) {
+    console.log(typeof(name));
+}
+
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+console.log(addExpenses.length);
+
+console.log('period =' + ' ' + period + ' ' + 'месяцев');
+console.log('цель заработвть' + ' '+ mission + ' ' + 'долларов');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = (money/30);
+console.log(budgetDay);
+
+
+ // Задание №2
+money = +prompt('Ваш ежемесячный доход?');
+
+//  Задание №3
+addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую');
+console.log(addExpenses.toLowerCase().split(', '));
+
+
+
+// Задание №4
+deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(deposit);
+
+// Задание №5
+let expenses1 = prompt('Введите обязательную статью расходов?');
+console.log(expenses1);
+
+let expenses2 = prompt('Введите обязательную статью расходов?');
+console.log(expenses2);
+
+let amount1 = +prompt('Во сколько это обойдется?');
+console.log(amount1);
+
+let amount2 = +prompt('Во сколько это обойдется?');
+console.log(amount2);
+
+// Задание №6
+ let budgetMonth = amount1 + amount2;
+ console.log('Бюджет на месяц:  ' + budgetMonth);
+
+// Задание №7
+let mission1 = mission / (money - budgetMonth);
+console.log('Цель будет достигнута за:  ' + Math.ceil(mission1));
+
+// Задание №8
+budgetDay = (budgetDay + budgetMonth);
+console.log('Бюджет на день:  ' + Math.floor(budgetDay));
+
+// Задание №9
+let getStatusIncome = function() {
+if (budgetDay >= 1200) {
+    return ('У вас высокий уровень дохода');
+} else if ((budgetDay >= 600) && (budgetDay <= 1200)) {
+    return ('У вас средний уровень дохода');
+} else if ((budgetDay < 600) && (budgetDay >= 0)) {
+    return ('К сожалению у вас уровень дохода ниже среднего');
+} else {
+    return('Что то пошло не так');
+} 
+}
+console.log(getStatusIncome());
+
+
+// Урок №4
+// Задание №1
+
+function getExpensesMonth(a, b) {
+let res = a + b;
+return res;
+}
+
+console.log(getExpensesMonth(amount1, amount2));
+
+
+// Задание №2
+function getAccumulatedMonth(a, b, c) {
+let res = a - b - c;
+return res;
+}
+
+console.log(getAccumulatedMonth(money, amount1, amount2));
+
+// Задание №3
+let accumulatedMonth = 0;
+function getAccumulatedMonth(a, b, c) {
+    let res = a - b - c;
+    return res;   
+}
+
+accumulatedMonth = getAccumulatedMonth(money, amount1, amount2);
+console.log(accumulatedMonth);
+
+
+// Задание №4
+function getTargetMonth(a, b) {
+let period = a / b;
+return period;
+}
+
+console.log(getTargetMonth(mission, accumulatedMonth));
