405e3674a14d92264fa395c8d93c4137ff0721a8
Boris Fokin <Befokin@mail.ru>
2020-04-06 20:54:32 +0300

    готово lesson_05 task_01

--- lesson_05 task_01/script.js
+++ lesson_05 task_01/script.js
@@ -1,70 +1,101 @@
 'use strict';
 
-let money = 100000;
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+};
+
+let money;
 let income = '5000';
 let addExpenses = 'Интернет, Такси, Коммуналка';
 let deposit = true;
-let mission = 10000;
+let mission = 100000;
 let period = 12;
+
+let start = function() {
+    
+    do {
+        money = prompt('Ваш месячный доход?');
+    }
+    while (!isNumber(money));
+
+};       
+
+start();
 
 let showTypeOf = function(name) {
     console.log(typeof(name));
-}
+};
 
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-console.log(addExpenses.length);
-
-console.log('period =' + ' ' + period + ' ' + 'месяцев');
-console.log('цель заработвть' + ' '+ mission + ' ' + 'долларов');
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = (money/30);
-console.log(budgetDay);
-
-
- // Задание №2
-money = +prompt('Ваш ежемесячный доход?');
-
-//  Задание №3
 addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую');
 console.log(addExpenses.toLowerCase().split(', '));
 
 
 
-// Задание №4
 deposit = confirm('Есть ли у вас депозит в банке?');
 console.log(deposit);
 
-// Задание №5
-let expenses1 = prompt('Введите обязательную статью расходов?');
-console.log(expenses1);
 
-let expenses2 = prompt('Введите обязательную статью расходов?');
-console.log(expenses2);
+let expenses = [];
+let expenses1;
 
-let amount1 = +prompt('Во сколько это обойдется?');
-console.log(amount1);
+let getExpensesMonth = function() {
+    let sum = 0;
 
-let amount2 = +prompt('Во сколько это обойдется?');
-console.log(amount2);
+    for (let i = 0; i < 2; i++) {
 
-// Задание №6
- let budgetMonth = amount1 + amount2;
- console.log('Бюджет на месяц:  ' + budgetMonth);
+        expenses[i] = prompt('Введите обязательную статью расходов?');
+        
+        do {
+            expenses1 = +prompt('Во сколько это обойдется?')
+            sum = sum + expenses1;
+        }
 
-// Задание №7
-let mission1 = mission / (money - budgetMonth);
-console.log('Цель будет достигнута за:  ' + Math.ceil(mission1));
+        while (!isNumber(expenses1)); 
+            
+    }
+      
+    console.log(expenses);
+    console.log(sum);
+    return sum;
+};
+ 
+let expensesAmount = getExpensesMonth();
+console.log('Расходы за месяц:  ' + expensesAmount);
+    
+    
 
-// Задание №8
-budgetDay = (budgetDay + budgetMonth);
-console.log('Бюджет на день:  ' + Math.floor(budgetDay));
+let getAccumulatedMonth = function() {
 
-// Задание №9
+return money - expensesAmount;
+};
+    
+let accumulatedMonth = getAccumulatedMonth();
+
+let res;
+let getTargetMonth = function() {
+    
+   res = mission / accumulatedMonth;
+       
+    return res;
+}
+getTargetMonth();
+    if (res > 0) {
+        console.log('Цель будет достигнута');
+    } else if (res < 0){
+        console.log('Цель не будет достигнута');
+    }
+
+
+let budgetDay = accumulatedMonth / 30;
+
+
 let getStatusIncome = function() {
 if (budgetDay >= 1200) {
     return ('У вас высокий уровень дохода');
@@ -77,42 +108,3 @@ if (budgetDay >= 1200) {
 } 
 }
 console.log(getStatusIncome());
-
-
-// Урок №4
-// Задание №1
-
-function getExpensesMonth(a, b) {
-let res = a + b;
-return res;
-}
-
-console.log(getExpensesMonth(amount1, amount2));
-
-
-// Задание №2
-function getAccumulatedMonth(a, b, c) {
-let res = a - b - c;
-return res;
-}
-
-console.log(getAccumulatedMonth(money, amount1, amount2));
-
-// Задание №3
-let accumulatedMonth = 0;
-function getAccumulatedMonth(a, b, c) {
-    let res = a - b - c;
-    return res;   
-}
-
-accumulatedMonth = getAccumulatedMonth(money, amount1, amount2);
-console.log(accumulatedMonth);
-
-
-// Задание №4
-function getTargetMonth(a, b) {
-let period = a / b;
-return period;
-}
-
-console.log(getTargetMonth(mission, accumulatedMonth));
