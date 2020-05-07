d7f9d60b7f575b4cfc01264bd53f1c95393db416
Boris Fokin <Befokin@mail.ru>
2020-05-07 22:39:04 +0300

    C:/Program Files/Git/

--- lesson_13 task_01/script.js
+++ lesson_13 task_01/script.js
@@ -4,27 +4,36 @@
             headerInput = document.querySelector('.header-input'),
             todoList = document.querySelector('.todo-list'),
             todoCompleted = document.querySelector('.todo-completed');
+          
 
     const todoData = [
             // Как добавить новое дело ? В этот массив добавить новое дело.
-            // {
-            //     value: 'Сварить кофе',
-            //     completed: false
-            // },
-            // {
-            //     value: 'Помыть посуду',
-            //     completed: true
-            // }
+            {
+                value: 'Сварить кофе',
+                completed: false,
+                removed: 0
+            },
+            {
+                value: 'Помыть посуду',
+                completed: true,
+                removed: 0
+            }
     ];
     // Сами дела необходимо где-то хранить. Создадим массив.
     // Каждый объект - это отдельное дело, которое мы туда добавили
 
     const render = function() {
-         // Добавим функцию, которая будет доблять эти дела на страницу (rander верстки)
+         // Добавим функцию, которая будет доблять эти дела на страницу 
+        //  (rander верстки)
+        // if(headerInput.value === ''){
+        //     alert('1');
+        // } else {
+      
+        headerInput.value = '';
         todoList.textContent = '';
         todoCompleted.textContent = '';
         // очищаем дело перед записью новых дел
-       
+         
             todoData.forEach(function(item){
                 console.log(item);
                 const li = document.createElement('li');
@@ -40,19 +49,26 @@
                      //    дела добавить на страницу   
                     // учитываем условие comleted
                     todoCompleted.append(li);
-                } else {
+                    } else {
                     todoList.append(li);    
                 }
 
                 const btnTodoComplete = li.querySelector('.todo-complete');
             //  Как менять статус дела? Выполнено оно или нет.
-            btnTodoComplete.addEventListener('click', function(){
+                
+                btnTodoComplete.addEventListener('click', function(){
                 item.completed = !item.completed;
                 render();
-            })
+                })
+
+                const btnTodoRemove = li.querySelector('.todo-remove');
+                btnTodoRemove.addEventListener('click', function(){
+                item.removed = 'delete';
+                });
                    
             });
-    };
+        
+        };
     
 
     todoControl.addEventListener('submit', function(event){
@@ -67,10 +83,9 @@
 
         todoData.push(newTodo);
         //  в todoData добавим новый объект   
-
         render();
         // Вызываем функцию render, чтобы у нас обновились все дела
-
+           
     });
     render();
  
