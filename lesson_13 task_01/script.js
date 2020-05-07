172d61d0c0b0e3978b2159d6a5ee6edc2183a596
Boris Fokin <Befokin@mail.ru>
2020-05-08 00:53:13 +0300

    C:/Program Files/Git/

--- lesson_13 task_01/script.js
+++ lesson_13 task_01/script.js
@@ -21,21 +21,28 @@
     ];
     // Сами дела необходимо где-то хранить. Создадим массив.
     // Каждый объект - это отдельное дело, которое мы туда добавили
+   
+    const deleteElem = function(deleteItem) {
+            data.todoData.forEach(function(item){
+                    if(todoData && item.value != deleteItem) {
+                    data.todoData  = todoData.push(deleteItem);
+                    }
+            });
+    
+
+    deleteElem();
 
     const render = function() {
          // Добавим функцию, которая будет доблять эти дела на страницу 
         //  (rander верстки)
-        // if(headerInput.value === ''){
-        //     alert('1');
-        // } else {
-      
+           
         headerInput.value = '';
         todoList.textContent = '';
         todoCompleted.textContent = '';
         // очищаем дело перед записью новых дел
          
-            todoData.forEach(function(item){
-                console.log(item);
+            data.todoData.forEach(function(item){
+                // console.log(item);
                 const li = document.createElement('li');
                 li.classList.add('todo-item');
                 
@@ -46,26 +53,29 @@
                                      '</div>';
                                                                   // в li будем добавлять верстку
                 if(item.completed) {
-                     //    дела добавить на страницу   
-                    // учитываем условие comleted
-                    todoCompleted.append(li);
-                    } else {
-                    todoList.append(li);    
+                        //    дела добавить на страницу   
+                        // учитываем условие comleted
+                        todoCompleted.append(li);
+                        } else {
+                        todoList.append(li);    
                 }
 
                 const btnTodoComplete = li.querySelector('.todo-complete');
-            //  Как менять статус дела? Выполнено оно или нет.
+                const btnTodoRemove = li.querySelector('.todo-remove');
                 
+               
                 btnTodoComplete.addEventListener('click', function(){
                 item.completed = !item.completed;
                 render();
                 })
-
-                const btnTodoRemove = li.querySelector('.todo-remove');
+                //  Как менять статус дела? Выполнено оно или нет.
+                
                 btnTodoRemove.addEventListener('click', function(){
-                item.removed = 'delete';
+               data.todoData = deleteItem(item.value);
+                render();
                 });
                    
+                // localStorage.setItem( JSON.stringify());
             });
         
         };
@@ -76,16 +86,14 @@
             // навесим событие submit на форму
             // (страницу будет перезагружаться - стандартное событие submit)
             // теперь страница не перезагружается
-            const newTodo = {
-                        value: headerInput.value,
-                        completed: false
-            };
+    const newTodo = {
+            value: headerInput.value,
+            completed: false
+    };
 
-        todoData.push(newTodo);
+    todoData.push(newTodo);
         //  в todoData добавим новый объект   
-        render();
+    render();
         // Вызываем функцию render, чтобы у нас обновились все дела
-           
     });
     render();
- 
