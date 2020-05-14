7630f293aa889a1f07f9b1db2baf40dacebd164b
Boris Fokin <Befokin@mail.ru>
2020-05-14 20:46:02 +0300

    C:/Program Files/Git/

---
+++ lesson_14 task_01/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function DomElement (selector, height, width, bg, fontSize){
+      this.selector = selector;
+      this.height = height;
+      this.width = width;
+      this.bg = bg;
+      this.fontSize = fontSize;
+};
+
+DomElement.prototype.createElem  = function() {
+      
+      if () {
+            const newElem = document.createElement('.div');
+      } else if () {
+            const newElem = document.createElement('#p')
+      }
+
+};
+
+function DomElement2(selector, height, width, bg, fontSize) {
+ 
+      DomElement.apply(this, arguments);
+};
+    
+DomElement2.prototype = Object.create(DomElement.prototype);
+     
+DomElement2.prototype.constructor =  DomElement2;
+
+const domElement = new DomElement();    
+const domElement3 = new DomElement2();
+domElement3.createElem();
